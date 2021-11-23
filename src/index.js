87f4323db2eb1cdafb7456d7ccd6419d2e36236e
mutinhiri <barnabmutinhiri@gmail.com>
2021-11-23 19:22:19 +0200

    Add image to the main project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import image from './deezer.png'
+
+const nav = document.getElementById('nav')
+const images = document.createElement('img')
+images.src = image
+nav.appendChild(images)
