b749b0907b9ae96dd90a8e5a5ea9c4a01f9a89a6
mutinhiri <barnabmutinhiri@gmail.com>
2021-11-23 20:31:31 +0200

    Add initial homepage setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+import image from './deezer.png'
+
+const nav = document.getElementById('nav')
+const main = document.getElementById('main-page')
+const images = document.createElement('img')
+images.src = image
+nav.appendChild(images)
+
+function musicInfo(id) {
+  const div = document.createElement('div')
+  div.innerHTML = `
+                  <div>
+                    <img class = info-image src= todo>
+                  </div>
+                  <p class="info-text">Todo</p>
+                  <button id="comments">todo</button>`
+}
