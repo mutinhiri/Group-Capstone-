3a235c1afeb2c10baee5781b8d6ee956ce473b69
mutinhiri <barnabmutinhiri@gmail.com>
2021-11-24 11:59:15 +0200

    Add changes to the Api

--- src/index.js
+++ src/index.js
@@ -7,12 +7,43 @@ const images = document.createElement('img')
 images.src = image
 nav.appendChild(images)
 
-function musicInfo(id) {
+function showInfo(show) {
   const div = document.createElement('div')
   div.innerHTML = `
                   <div>
-                    <img class = info-image src= todo>
+                    <img class ="info-image" src="${show.image}">
                   </div>
-                  <p class="info-text">Todo</p>
-                  <button id="comments">todo</button>`
+                  <p class="info-text">${show}</p>
+                  <button id="comments-${show.id}">Comments</button>
+                  <button id ="reservation-${show.id}">Reservation</button>
+                  `
+
+main.appendChild(div)
+
+const commentBtn = document.getElementById('comments')
+const reservationBtn = document.getElementById('reservation')
+
+// commentBtn.addEventListener('click', function (){
+// //coments section
+// })
+
+// reservationBtn.addEventListener('click', function(){
+//   // reservation section
+// })
+};
+
+const getShowInfo = async () => {
+  let url = 'https://api.tvmaze.com/shows/${show}';
+  const response = await fetch(url, {
+    headers: { 'x-api-key': process.env.API_KEY },
+  
+  })
+  return response.json()
+  
 }
+
+getShowInfo().then((e) =>{
+  e.forEach((show) => showInfo(show)) 
+})
+
+showInfo(30)
