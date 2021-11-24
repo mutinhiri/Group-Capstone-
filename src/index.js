72bee5cdc053cae8affd2d44c6b22cde031175ea
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-11-24 17:20:27 +0100

    Add reservation popup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+import test from './reservations';
+import { trying } from './reservations';
+test();
+// getShows();
+
---
+++ src/reservations.js
@@ -0,0 +1,36 @@
+const test = () => {
+    const Btn = document.getElementById('btn');
+    Btn.addEventListener('click', () => {
+
+        const getShows = async () => {
+          const Url = 'https://api.tvmaze.com/singlesearch/shows?q=friends';
+          const response = await fetch(Url, { method: 'GET' });
+          const show = await response.json();
+          return show;
+        };
+        getShows().then((show) => {
+          trying(show.image.medium, show.rating.average);
+        });
+    } 
+    )};
+
+const trying = (image, info)  => {
+    const popupRes = document.getElementById('reserve');
+    popupRes.classList = 'popup-reservation-wraper';
+    const text = document.createElement('p');
+    const img = document.createElement('img');
+    img.setAttribute("alt", "preview")
+    img.setAttribute("src", `${image}`);
+    popupRes.innerHTML = `
+    
+    <h1>${info}</h1>
+    `
+    popupRes.appendChild(img);
+    popupRes.appendChild(text);
+}
+
+
+export default  test; 
+export { trying };
+
+ 
