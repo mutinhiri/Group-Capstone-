087474460b27bf96f8bfccfd18c7d5f0af866db2
mutinhiri <barnabmutinhiri@gmail.com>
2021-11-24 16:54:15 +0200

    Add linter configs

--- src/index.js
+++ src/index.js
@@ -28,7 +28,9 @@ function createCard(actor) {
 }
 
 const getActorsData = async () => {
-  const url = 'https://api.tvmaze.com/people?page=4'
+  // const url = 'https://api.tvmaze.com/people?page=4'
+  const url = 'https://api.tvmaze.com/shows'
+
   const response = await fetch(url, {
   });
   return response.json();
