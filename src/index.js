aa7b4507b3935846ad8838291b35915785ea620a
mutinhiri <barnabmutinhiri@gmail.com>
2021-11-24 14:52:39 +0200

    Add tv maze actors

--- src/index.js
+++ src/index.js
@@ -1,49 +1,39 @@
-import './style.css';
-import image from './deezer.png'
+const mainSection = document.getElementById('main-page');
 
-const nav = document.getElementById('nav')
-const main = document.getElementById('main-page')
-const images = document.createElement('img')
-images.src = image
-nav.appendChild(images)
+function createCard(actor) {
+  const card = document.createElement('div');
+  card.classList = 'main-section-card';
+  card.innerHTML = `
+    <div class="card-image-container">
+      <img class="card-image" src="${actor.image.medium}" alt="">
+    </div>
+    <h2 class="card-title">${actor.name}</h2>
+    <button id="comments-button-${actor.id}" class="comments">Comments</button>
+    <button id="reservations-button-${actor.id}" class="reservations">Reservations</button>
+  `;
 
-function showInfo(show) {
-  const div = document.createElement('div')
-  div.innerHTML = `
-                  <div>
-                    <img class ="info-image" src="${show.image}">
-                  </div>
-                  <p class="info-text">${show}</p>
-                  <button id="comments-${show.id}">Comments</button>
-                  <button id ="reservation-${show.id}">Reservation</button>
-                  `
+  mainSection.appendChild(card);
 
-main.appendChild(div)
+  const commentsButton = document.getElementById(`comments-button-${actor.id}`);
+  commentsButton.addEventListener('click', () => {
+    // Comments
+  });
 
-const commentBtn = document.getElementById('comments')
-const reservationBtn = document.getElementById('reservation')
-
-// commentBtn.addEventListener('click', function (){
-// //coments section
-// })
-
-// reservationBtn.addEventListener('click', function(){
-//   // reservation section
-// })
-};
-
-const getShowInfo = async () => {
-  let url = 'https://api.tvmaze.com/shows/${show}';
-  const response = await fetch(url, {
-    headers: { 'x-api-key': process.env.API_KEY },
-  
-  })
-  return response.json()
-  
+  const reservationsButton = document.getElementById(`reservations-button-${actor.id}`);
+  reservationsButton.addEventListener('click', () => {
+    // Reservations
+  });
 }
 
-getShowInfo().then((e) =>{
-  e.forEach((show) => showInfo(show)) 
-})
+const getActorsData = async () => {
+  const url = 'https://api.tvmaze.com/people?page=4'
+  const response = await fetch(url, {
+  });
+  return response.json();
+};
 
-showInfo(30)
+getActorsData().then((list) => {
+  for (let i = 0; i <6; i++){
+    list.forEach((actor) => createCard(actor));
+  }
+});
