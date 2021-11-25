4c784e7156a8775dd8b3e7ba8b070be54d0c37a1
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-11-25 15:34:08 +0100

    Add form for reservations

--- src/reservations.js
+++ src/reservations.js
@@ -1,5 +1,3 @@
-
-
 // const reservationPopup = () => {
 //     const Btn = document.getElementById('btn');
 //     Btn.addEventListener('click', () => {
@@ -13,42 +11,63 @@
 //         getShows().then((show) => {
 //           displayPopup(show.image.medium, show.rating.average);
 //         });
-//     } 
+//     }
 //     )};
-
-const displayPopup = (image, info)  => {
-    const popupRes = document.getElementById('reserve');
-    popupRes.classList = 'popup-reservation-wraper';
-    document.getElementById("reserve").style.display="block"; 
-    const text = document.createElement('p');
-    const img = document.createElement('img');
-    img.setAttribute("alt", "preview")
-    img.setAttribute("src", `${image}`);
-    popupRes.innerHTML = `
+/* eslint-disable */
+const displayPopup = (image, info) => {
+  const popupRes = document.getElementById('reserve');
+  popupRes.classList = 'popup-reservation-wraper';
+  document.getElementById('reserve').style.display = 'block';
+  const text = document.createElement('p');
+  const img = document.createElement('img');
+  img.setAttribute('alt', 'preview');
+  img.setAttribute('src', `${image}`);
+  popupRes.innerHTML = `
     <a id="close">X</a>
     <h1>${info}</h1>
-    `
-    const anchor =  document.getElementById('close');
-    popupRes.appendChild(img);
-    popupRes.appendChild(text);
+    `;
+  const anchor = document.getElementById('close');
+  popupRes.appendChild(img);
+  popupRes.appendChild(text);
 
-
-    anchor.addEventListener('click',  () => {
+  anchor.addEventListener('click', () => {
     popupRes.style.display = 'none';
-    }
-
-    )
-}
+  });
+};
 
 
 
+// Form reservation
+const addReservations = document.createElement('form');
+addReservations.classList.add('reservations-form');
+const formHeader = document.createElement('h2');
+formHeader.textContent = 'Reserve you Blueray'
+formHeader.classList.add('form-header');
+const name = document.createElement('input');
+name.classList.add('name');
+name.type = 'text';
+name.placeholder = 'your name';
+const startDate = document.createElement('input');
+startDate.classList.add('start-date');
+startDate.type = 'date';
+startDate.placeholder ='Enter start date';
+const endDate = document.createElement('input');
+endDate.classList.add('end-date');
+endDate.type = 'date';
+endDate.placeholder ='Enter end date';
+const submit = document.createElement('button');
+submit.classList.add('submit');
+submit.textContent = 'Reserve';
+submit.type ='submit';
+
+addReservations.appendChild(formHeader);
+addReservations.appendChild(name);
+addReservations.appendChild(startDate);
+addReservations.appendChild(endDate);
+addReservations.appendChild(submit);
 
 
 
-
-
-// export default  reservationPopup; 
-export { displayPopup };
+// export default  reservationPopup;
+// export default displayPopup;
 // export default getShows;
-
- 
