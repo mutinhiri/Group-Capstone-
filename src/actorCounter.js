e2d81c5e291ff1ad48f275d437f5960f2e42fbc2
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-03 16:55:07 +0200

    Add style  and neccessary changes

--- src/actorCounter.js
+++ src/actorCounter.js
@@ -1,6 +1,6 @@
 function actorCounter(list) {
   const counter = document.getElementById('actor-count');
-  counter.innerHTML = `Actor Count (${list.splice(-30).length})`;
+  counter.innerHTML = `Actor Count (${list.splice(-20).length})`;
 }
 
 module.exports = actorCounter;
